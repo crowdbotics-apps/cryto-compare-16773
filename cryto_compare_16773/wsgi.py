75580b539cdad898b391fe6ad3c795b971ddf2eb
Crowdbotics <team@crowdbotics.com>
2020-05-08 22:31:54 +0000

    Update models

--- cryto_compare_16773/wsgi.py
+++ cryto_compare_16773/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cryto_compare_16773.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cryto_compare_16773.settings")
 
 application = get_wsgi_application()
